4ac33946430f42f70607a70d6796879afe2fbbcb
CogEpitome <iacobi.jonas@gmail.com>
2018-02-07 22:29:07 +0100

    database update

---
+++ src/main/java/kthknugarna/unrut/view/RecruiterView.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kthknugarna.unrut.view;
+
+/**
+ *
+ * @author Jonas
+ * 
+ * List applications
+ * -List x (less than 1000 || default_value) apps based on time period, date, competence, name
+ * -Show name, surname, date
+ * Sort applications
+ * 
+ * Show application
+ * -show more detailed view, along with accept/deny
+ * Accept applicant
+ * Deny applicant
+ */
+public class RecruiterView {
+    
+}
