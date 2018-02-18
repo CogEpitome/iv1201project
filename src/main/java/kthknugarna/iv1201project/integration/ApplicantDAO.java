9b0fdd4e9b604986f1688b2bb05c4a0dcb1f57e2
Superboven <antonat@kth.se>
2018-02-18 01:41:54 +0100

    Commented ApplicantDAO

--- src/main/java/kthknugarna/iv1201project/integration/ApplicantDAO.java
+++ src/main/java/kthknugarna/iv1201project/integration/ApplicantDAO.java
@@ -27,6 +27,7 @@ import kthknugarna.iv1201project.model.Input;
 import kthknugarna.iv1201project.model.dto.InputDTO;
 
 /**
+ * ApplicantDAO is a data access object that stores and retrieves data from a database.
  *
  * @author Jonas
  * @author Anton
@@ -39,6 +40,10 @@ public class ApplicantDAO {
     @PersistenceContext(unitName = "applicantPU")
     private EntityManager em;
 
+    /**
+     * Method to test the storing of a person.
+     * @param inputDTO DTO for the input class
+     */
     public void testStore(InputDTO inputDTO){
         try{
             Person person = new Person((long)0, inputDTO.getFirstName(), inputDTO.getSurname(),"10", inputDTO.getPassword(), inputDTO.getUsername(), getRole(0));
@@ -48,7 +53,15 @@ public class ApplicantDAO {
         }
     }
     
-    
+    /**
+     * Returns a Person object for authentication. the username argument is entered 
+     * from the client, and if the username exists the person that holds the 
+     * username is returned. If no person has the username, the method 
+     * returns null.
+     * @param username the chosen name of a registered user
+     * @return         the person with the specified username
+     * @see            Person
+     */
     public Person getPerson(String username){
         TypedQuery<Person> query = em.createNamedQuery("Person.findByUsername", Person.class);
         query.setParameter("username", username);
@@ -57,23 +70,57 @@ public class ApplicantDAO {
         return person.get(0);
         else return null;
     }
-    
+    /**
+     * Returns a Person object based on a personId. the id argument matches the
+     * personId of a competence profile.
+     * @param id personId belonging to a competenceProfile
+     * @return   the person with the specified id
+     * @see      Person
+     */
     public Person getPerson(long id){
         return em.find(Person.class, id);
     }
     
+    /**
+     * Returns a competenceProfile object for recruiting purposes. The id
+     * argument is the id of the profile to retrieve.
+     * @param id the id of the competence profile
+     * @return   the competence profile with specified id
+     * @see      CompetenceProfile
+     */
     public CompetenceProfile getCompetenceProfile(long id){
         return em.find(CompetenceProfile.class, id);
     }
-    
+    /**
+     * Returns a Role object for web navigation and verification of users.
+     * The id argument is either 1 or 0, which corresponds to recruiter or 
+     * applicant.
+     * @param id the id of a role
+     * @return   the role with the specified id
+     * @see      Role
+     */
     public Role getRole(long id){
         return em.find(Role.class, id);
     }
     
+    /**
+     * Returns a Competence object to retrieve the name of a competence tied
+     * to a specified id. The id argument is the id of the requested competence.
+     * @param id the id of a Competence
+     * @return   the competence with the specified id
+     * @see      Competence
+     */
     public Competence getCompetence(long id){
         return em.find(Competence.class, id);
     }
     
+    /**
+     * Returns an Availability object for recruiting purposes. The id argument
+     * refers to the id of a person.
+     * @param id the id of Person
+     * @return   the availability period of an applicant
+     * @see      Availability
+     */
     public Availability getAvailability(long id){
         return em.find(Availability.class, id);
     }
