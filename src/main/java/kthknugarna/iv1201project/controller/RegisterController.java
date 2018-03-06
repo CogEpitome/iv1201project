5f065f58444f0f2ca73bbaf5d36df81fbb02827d
CogEpitome <iacobi.jonas@gmail.com>
2018-03-06 17:24:54 +0100

    Add JavaDoc and Error handling for register and login pipelines

--- src/main/java/kthknugarna/iv1201project/controller/RegisterController.java
+++ src/main/java/kthknugarna/iv1201project/controller/RegisterController.java
@@ -4,19 +4,20 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.transaction.Transactional;
 import kthknugarna.iv1201project.integration.ApplicantDAO;
 import kthknugarna.iv1201project.model.Person;
 import kthknugarna.iv1201project.model.VerificationHandler;
 import kthknugarna.iv1201project.model.dto.InputDTO;
 
 /**
- *
  * @author Jonas
  * @author Anton
  * @author Benjamin
+ * 
+ * The controller class connected to the RegisterView class. 
+ * Handles all interactions between the view and other layers when registering a new account.
+ * 
+ * @see RegisterView
  */
 @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 @Stateless
@@ -26,22 +27,34 @@ public class RegisterController {
     
     /**
      * Registers a user if the username is not already taken
-     * @param input DTO with registration input information from the user
-     * @return success message or error message
+     * @param       input DTO with registration input information from the user
+     * @return      String with success message or error message
+     * @throws java.lang.Exception
      */
-    public String register(InputDTO input){
-        Person person = getPerson(input.getUsername());
-        if(person != null)
-            return "A user with the username :"+person.getUsername()+" already exists. Please try a different username.";
-        else {
-            person = new Person(input.getFirstName(), input.getSurname(), input.getDateOfBirth(), input.getPassword(), input.getUsername(), dao.getRole(0));
-            dao.persist(person);
-            return "success";
+    public String register(InputDTO input) throws Exception{
+        try{
+            Person person = getPerson(input.getUsername());
+            if(person != null)
+                return "A user with the username :"+person.getUsername()+" already exists. Please try a different username.";
+            else {
+                person = new Person(input.getFirstName(), input.getSurname(), input.getDateOfBirth(), input.getPassword(), input.getUsername(), dao.getRole(0));
+                dao.persist(person);
+                return "success";
+            }
+        } catch (Exception e){
+            throw new Exception(e);
         }
             
     }
     
-    public Person getPerson(String username){
+    /**
+     * Returns a Person object from the database based on their username.
+     * @param username
+     * @return  The Person matching the username, null if none was found
+     * @throws  Exception 
+     * @see     Person
+     */
+    public Person getPerson(String username) throws Exception{
         return dao.getPerson(username);
     }
     
--- src/main/java/kthknugarna/iv1201project/integration/ApplicantDAO.java
+++ src/main/java/kthknugarna/iv1201project/integration/ApplicantDAO.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kthknugarna.iv1201project.integration;
 
 import kthknugarna.iv1201project.model.Role;
 import kthknugarna.iv1201project.model.Person;
 import java.util.List;
+import javax.ejb.EJBException;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
@@ -19,44 +15,30 @@ import kthknugarna.iv1201project.model.Availability;
 import kthknugarna.iv1201project.model.Competence;
 import kthknugarna.iv1201project.model.CompetenceProfile;
 import kthknugarna.iv1201project.model.Status;
-import kthknugarna.iv1201project.model.dto.InputDTO;
 
 /**
- * ApplicantDAO is a data access object that stores and retrieves data from a database.
- *
  * @author Jonas
  * @author Anton
  * @author Benjamin
+ * 
+ * ApplicantDAO is a data access object that stores and retrieves data from the Applications database.
  */
-
 @TransactionAttribute(TransactionAttributeType.MANDATORY)
 @Stateless
 public class ApplicantDAO {
     @PersistenceContext(unitName = "applicantPU")
     private EntityManager em;
-
-    /**
-     * Method to test the storing of a person.
-     * @param inputDTO DTO for the input class
-     */
-    public void testStore(InputDTO inputDTO){
-        try{
-            Person person = new Person(inputDTO.getFirstName(), inputDTO.getSurname(),"10", inputDTO.getPassword(), inputDTO.getUsername(), getRole(0));
-            em.persist(person);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-    }
     
     /**
-     * TODO: add exception handling.
      * Method to persist an entity to the database.
      * @param obj The object to persist
+     * @throws java.lang.Exception
      */
-    public void persist(Object obj){
+    public void persist(Object obj) throws Exception{
         try{
             em.persist(obj);
-        } catch (Exception e){
+        } catch (EJBException ee){
+            throw new Exception(ee);
         }
     }
     
@@ -68,46 +50,68 @@ public class ApplicantDAO {
      * @param username the chosen name of a registered user
      * @return         the person with the specified username
      * @see            Person
+     * @throws java.lang.Exception
      */
-    public Person getPerson(String username){
-        TypedQuery<Person> query = em.createNamedQuery("Person.findByUsername", Person.class);
-        query.setParameter("username", username);
-        List<Person> person = query.getResultList();
-        if(!person.isEmpty())
-        return person.get(0);
-        else return null;
+    public Person getPerson(String username) throws Exception{
+        try{
+            TypedQuery<Person> query = em.createNamedQuery("Person.findByUsername", Person.class);
+            query.setParameter("username", username);
+            List<Person> person = query.getResultList();
+            if(!person.isEmpty())
+            return person.get(0);
+            else return null;
+        } catch (EJBException ee){
+            throw new Exception(ee);
+        }
     }
+    
     /**
      * Returns a Person object based on a personId. the id argument matches the
      * personId of a competence profile.
      * @param id personId belonging to a competenceProfile
      * @return   the person with the specified id
+     * @throws java.lang.Exception
      * @see      Person
      */
-    public Person getPerson(long id){
-        return em.find(Person.class, id);
+    public Person getPerson(long id) throws Exception{
+        try{
+            return em.find(Person.class, id);
+        } catch (EJBException ee){
+            throw new Exception(ee);
+        }
     }
     
     /**
-     * Returns a competenceProfile object for recruiting purposes. The id
+     * Returns a CompetenceProfile object for recruiting purposes. The id
      * argument is the id of the profile to retrieve.
      * @param id the id of the competence profile
      * @return   the competence profile with specified id
+     * @throws java.lang.Exception
      * @see      CompetenceProfile
      */
-    public CompetenceProfile getCompetenceProfile(long id){
-        return em.find(CompetenceProfile.class, id);
+    public CompetenceProfile getCompetenceProfile(long id) throws Exception{
+        try{
+            return em.find(CompetenceProfile.class, id);
+        } catch (EJBException ee){
+            throw new Exception(ee);
+        }
     }
+    
     /**
      * Returns a Role object for web navigation and verification of users.
      * The id argument is either 1 or 0, which corresponds to recruiter or 
      * applicant.
      * @param id the id of a role
      * @return   the role with the specified id
+     * @throws java.lang.Exception
      * @see      Role
      */
-    public Role getRole(long id){
-        return em.find(Role.class, id);
+    public Role getRole(long id) throws Exception{
+        try{
+            return em.find(Role.class, id);
+        } catch (EJBException ee){
+            throw new Exception(ee);
+        }
     }
     
     /**
@@ -115,19 +119,36 @@ public class ApplicantDAO {
      * to a specified id. The id argument is the id of the requested competence.
      * @param id the id of a Competence
      * @return   the competence with the specified id
+     * @throws java.lang.Exception
      * @see      Competence
      */
-    public Competence getCompetence(long id){
+    public Competence getCompetence(long id) throws Exception{
+        try{
         return em.find(Competence.class, id);
+        } catch (EJBException ee){
+            throw new Exception(ee);
+        }
     }
     
-    public Competence getCompetence(String name){
-        TypedQuery<Competence> query = em.createNamedQuery("Competence.findByName", Competence.class);
-        query.setParameter("name", name);
-        List<Competence> competence = query.getResultList();
-        if(!competence.isEmpty())
-        return competence.get(0);
-        else return null;
+    /**
+     * Returns a Competence object to retrieve the name of a competence tied
+     * to a specified name. The name argument is the name of the requested competence.
+     * @param name the name of a Competence
+     * @return   the competence with the specified name
+     * @throws java.lang.Exception
+     * @see      Competence
+     */
+    public Competence getCompetence(String name) throws Exception{
+        try{
+            TypedQuery<Competence> query = em.createNamedQuery("Competence.findByName", Competence.class);
+            query.setParameter("name", name);
+            List<Competence> competence = query.getResultList();
+            if(!competence.isEmpty())
+            return competence.get(0);
+            else return null;
+        } catch (Exception e){
+            throw new Exception(e);
+        }
     }
     
     /**
@@ -135,34 +156,49 @@ public class ApplicantDAO {
      * refers to the id of a person.
      * @param id the id of Person
      * @return   the availability period of an applicant
+     * @throws java.lang.Exception
      * @see      Availability
      */
-    public Availability getAvailability(long id){
-        return em.find(Availability.class, id);
+    public Availability getAvailability(long id) throws Exception{
+        try{
+            return em.find(Availability.class, id);
+        } catch (Exception e){
+            throw new Exception(e);
+        }
     }
     
     /**
      * Returns a list of all Availability objects corresponding to a person
      * @param person the person the availability corresponds to
      * @return   the availability period of an applicant
+     * @throws java.lang.Exception
      * @see      Availability
      */
-    public List<Availability> getAvailabilityList(Person person){
-        TypedQuery<Availability> query = em.createNamedQuery("Availability.findPersonId", Availability.class);
-        query.setParameter(1, person);
-        return query.getResultList();
+    public List<Availability> getAvailabilityList(Person person) throws Exception{
+        try{
+            TypedQuery<Availability> query = em.createNamedQuery("Availability.findPersonId", Availability.class);
+            query.setParameter(1, person);
+            return query.getResultList();
+        } catch (Exception e){
+            throw new Exception(e);
+        }
     }
     
     /**
      * Returns a list of all CompetenceProfile objects corresponding to a person
      * @param person the person the competence profile corresponds to
      * @return   the availability period of an applicant
+     * @throws java.lang.Exception
      * @see      Availability
      */
-    public List<CompetenceProfile> getCompetenceProfileList(Person person){
+    public List<CompetenceProfile> getCompetenceProfileList(Person person) throws Exception{
+        try{
         TypedQuery<CompetenceProfile> query = em.createNamedQuery("CompetenceProfile.findPersonId", CompetenceProfile.class);
         query.setParameter(1, person);
         return query.getResultList();
+        } catch (Exception e){
+            throw new Exception(e);
+        }
     }
     
     /**
@@ -170,10 +206,15 @@ public class ApplicantDAO {
      * refers to the id of a person.
      * @param id the id of Person
      * @return   the availability period of an applicant
+     * @throws java.lang.Exception
      * @see      Availability
      */
-    public Status getStatus(long id){
+    public Status getStatus(long id) throws Exception{
+        try{
         return em.find(Status.class, id);
+        } catch (EJBException e){
+            throw new Exception(e);
+        }
     }
     
     /**
@@ -181,19 +222,29 @@ public class ApplicantDAO {
      * refers to the id of an application.
      * @param id the id of Application
      * @return   the Application object mathing the id
+     * @throws java.lang.Exception
      * @see      Application
      */
-    public Application getApplication(long id){
-        return em.find(Application.class, id);
+    public Application getApplication(long id) throws Exception{
+        try{
+            return em.find(Application.class, id);
+        } catch (EJBException ee){
+            throw new Exception(ee);
+        }
     }
     
     /**
-     * Returns an Application object for displaying to the recruiters.
-     * @return   the Application object mathing the id
+     * Returns a list of Application objects for displaying to the recruiters.
+     * @return   a List of all Application objects
+     * @throws java.lang.Exception
      * @see      Application
      */
-    public List<Application> getApplicationList(){
+    public List<Application> getApplicationList() throws Exception{
+        try{
         TypedQuery<Application> query = em. createNamedQuery("Application.findAll", Application.class);
         return query.getResultList();
+        } catch(Exception e){
+            throw new Exception(e);
+        }
     }
 }
